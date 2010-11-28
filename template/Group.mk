ab84b0bbed2ed96d2ad0baa35b89f347859cd3b4
j8takagi <j8takagi@nifty.com>
2010-11-29 01:57:55 +0900

    テストごとに詳細ログdeteil.logを作成するよう仕様追加

--- template/Group.mk
+++ template/Group.mk
@@ -13,102 +13,8 @@
 
 SHELL = /bin/sh
 
-######################################################################
-# テストグループの定義
-######################################################################
-
 include Define.mk
-
-# グループディレクトリー
-GROUP_DIR := $(shell pwd)
-
-# グループ名。ディレクトリ名から取得
-GROUP := $(notdir $(GROUP_DIR))
-
-# テスト名。カレントディレクトリー内の、名前が大文字または.以外で始まるディレクトリー
-TESTS = $(notdir $(shell find -maxdepth 1 -name "[^A-Z.]*" -type d))
-
-# テストグループログファイル
-GROUP_LOG_FILE := $(shell echo $(GROUP) | tr '[a-z]' '[A-Z]').log
-
-# テストグループレポートファイル
-GROUP_REPORT_FILE := Report.log
-
-# テストグループ実行時間ファイル
-GROUP_TIME_FILE := $(shell echo $(GROUP) | tr '[a-z]' '[A-Z]')_time.log
-
-# グループで、テスト時に作成されるファイル群
-GROUP_TEMP_FILES := $(GROUP_LOG_FILE) $(GROUP_REPORT_FILE) $(GROUP_TIME_FILE)
-
-# テストごとのログファイル
-TEST_LOG_FILES := $(foreach test,$(TESTS),$(test)/$(LOG_FILE))
-
-# 指定したディレクトリーを作成
-# 用例: $(call create_dir,name)
-define create_dir
-    $(call chk_var_null,$1)
-    $(call chk_file_ext,$1)
-    $(MKDIR) $1
-endef
-
-# テストごとのMakefileを作成
-# 用例: $(call create_makefile,file)
-define create_makefile
-    $(RM) $1
-    $(foreach mkfile,$(DEF_FILE) $(TEST_MAKEFILE),$(ECHO) "include ../$(mkfile)" >>$1; )
-endef
-
-# テストごとのファイルをグループファイルに出力
-# 引数は、テストのリスト、グループファイル、テストファイル
-# 用例: $(call group_log,files_test_log,file_group_log)
-define group_log
-    $(foreach target,$1,$(call group_log_each,$(target),$2))
-endef
-
-# テストのログファイルをグループログファイルに出力。引数は、テスト、グループログファイル
-# 用例: $(call group_log_each,file_test_log,file_group_log)
-define group_log_each
-    if test -s $1; then $(CAT) $1 >>$2; else $(ECHO) $(dir $1)": no log" >>$2; fi
-    echo >>$2;
-
-endef
-
-# 成功したテストの数。テストグループログファイルから取得
-SUCCESS_TEST = $(shell grep "^[^A-Z.].*: Test Success" $(GROUP_LOG_FILE) | wc -l)
-
-# 失敗したテストの数。テストグループログファイルから取得
-FAIL_TEST = $(shell grep "^[^A-Z.].*: Test Failure" $(GROUP_LOG_FILE) | wc -l)
-
-# すべてのテストの数
-ALL_TEST = $(shell expr $(SUCCESS_TEST) + $(FAIL_TEST))
-
-# テストごとの実行時間ファイル
-TEST_TIME_FILES := $(foreach test,$(TESTS),$(test)/$(TIME_FILE))
-
-# テストの結果を、グループログファイルを元にレポート。
-# 引数は、グループログファイル
-# 用例: $(call group_report,name,file_log,file_report)
-define group_report
-    $(ECHO) "$1: $(SUCCESS_TEST) / $(ALL_TEST) tests passed. Details in $(GROUP_DIR)/$2" >$3;
-    if test $(FAIL_TEST) -eq 0; then $(ECHO) "$1: All tests are succeded." >>$3; fi
-endef
-
-# リストで指定したディレクトリーでmakeを実行
-# 用例: $(call make_tests,list_dir,target)
-define make_tests
-    $(foreach dir,$1,$(call make_test_each,$(dir),$2))
-endef
-
-# 指定したディレクトリーでMakeを実行
-# 用例: $(call make_test_each,tests,target)
-define make_test_each
-    $(MAKE) $2 -sC $1;
-
-endef
-
-######################################################################
-# ターゲット
-######################################################################
+include Define_group.mk
 
 .PHONY: check checkall time create clean cleantime
 
@@ -120,11 +26,11 @@ time: cleantime $(GROUP_TIME_FILE)
 
 create:
 	@$(call create_dir,$(TEST))
-	@$(call create_makefile,$(TEST)/$(MAKEFILE))
+	@$(call create_makefile,$(TEST)/$(MAKEFILE),$(TEST_MAKEFILES))
 
 clean:
 	@$(call make_tests,$(TESTS),$@)
-	@$(RM) $(GROUP_TEMP_FILES)
+	@$(RM) $(GROUP_RES_FILES)
 
 cleantime:
 	@$(call make_tests,$(TESTS),$@)
@@ -137,7 +43,7 @@ $(GROUP_LOG_FILE): $(TEST_LOG_FILES)
 	@$(call group_log,$^,$@)
 
 $(TEST_LOG_FILES):
-	@$(MAKE) $(MAKECMDGOALS) -sC $(dir $@)
+	@$(MAKE) $(MAKECMDGOALS) -sC $(dir $@) NODISP=1
 
 $(GROUP_TIME_FILE): $(TEST_TIME_FILES)
 	@$(call group_log,$^,$@)
