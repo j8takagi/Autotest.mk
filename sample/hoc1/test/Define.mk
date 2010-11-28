a52765f687eff6aa1e9cc1a9262d32f0d3baa798
j8takagi <j8takagi@nifty.com>
2010-11-29 02:04:21 +0900

    テストのサンプルに新しいテンプレートを反映し、整理

--- sample/hoc1/test/Define.mk
+++ sample/hoc1/test/Define.mk
@@ -4,22 +4,93 @@
 ifndef DEFINE_INCLUDED
 DEFINE_INCLUDED = 1
 
+# 現在の日時
+DATE = $(shell date +"%F %T")
+
+# 現在のディレクトリー
+CURRDIR = $(shell pwd)
+
+# Makefile
+MAKEFILE := Makefile
+
+######################################################################
+# コマンド
+######################################################################
+
+CP ?= cp
+
+CAT ?= cat
+
+MKDIR ?= mkdir
+
+RM ?= rm -f
+
+ECHO ?= echo
+
+TIME ?= /usr/bin/time --quiet
+
+DIFF ?= diff -c
+
+DEV_NULL ?= /dev/null
+
+CHMOD ?= chmod
+
+GREP ?= grep
+
+LINECOUNT ?= wc -l
+
+FIND ?= find
+
+TR ?= tr
+
+EXPR ?= expr
+######################################################################
+# テストグループとテストでの共通マクロ
+######################################################################
+
+# chk_var_null: 引数がNULLの場合、エラー
+# 用例: $(call chk_var_null,var)
+define chk_var_null
+    $(if $1,,$(error NULL argument))
+endef
+
+# chk_file_ext: 指定されたファイルが実在する場合、エラー
+# 用例: $(call chk_file_ext,file)
+define chk_file_ext
+    $(if $(wildcard $1),$(error $1 exists in $(CURRDIR)))
+endef
+
+# chk_file_notext: 指定されたファイルが実在しない場合、エラー
+# 用例: $(call chk_file_notext,file)
+define chk_file_notext
+    $(if $(wildcard $1),,$(error $1 not exists in $(CURRDIR)))
+endef
+
 ######################################################################
 # テストグループのディレクトリー
 ######################################################################
 
-# テストグループとテストの両方で使う変数を定義したファイル
+# テストグループとテストの変数を定義したMakefile
 DEF_FILE := Define.mk
 
-# テストのMakefileにインクルードするファイル
+# テストグループの変数を定義したMakefile
+DEF_GROUP_FILE := Define_group.mk
+
+# テストの変数を定義したMakefile
+DEF_TEST_FILE := Define_test.mk
+
+# テストのターゲットを定義したMakefile
 TEST_MAKEFILE := Test.mk
+
+# すべてのMakefile群
+MAKEFILES := $(DEF_FILE) $(DEF_GROUP_FILE) $(DEF_TEST_FILE) $(TEST_MAKEFILE)
+
+# テストごとのMakefileでインクルードするMakefile群
+TEST_MAKEFILES := $(DEF_FILE) $(DEF_TEST_FILE) $(TEST_MAKEFILE)
 
 ######################################################################
 # テストのディレクトリー
 ######################################################################
-
-# Makefile
-MAKEFILE := Makefile
 
 # テストコマンドファイル
 CMD_FILE := cmd
@@ -45,51 +116,10 @@ LOG_FILE := test.log
 # 実行時間ファイル
 TIME_FILE := time.log
 
-# 現在の日時
-DATE = $(shell date +"%F %T")
+# テスト詳細レポートファイル
+DETAIL_FILE := detail.log
 
-######################################################################
-# コマンド
-######################################################################
-
-CP ?= cp
-
-CAT ?= cat
-
-MKDIR ?= mkdir
-
-RM ?= rm -f
-
-ECHO ?= echo
-
-TIME ?= /usr/bin/time --quiet
-
-DIFF ?= diff -c
-
-DEV_NULL ?= /dev/null
-
-CHMOD ?= chmod
-
-######################################################################
-# マクロ
-######################################################################
-
-# chk_var_null: 引数がNULLの場合、エラー
-# 用例: $(call chk_var_null,var)
-define chk_var_null
-    $(if $1,,$(error NULL argument))
-endef
-
-# chk_file_ext: 指定されたファイルが実在する場合、エラー
-# 用例: $(call chk_file_ext,file)
-define chk_file_ext
-    $(if $(wildcard $1),$(error $1 exists in $(CURRDIR)))
-endef
-
-# chk_file_notext: 指定されたファイルが実在しない場合、エラー
-# 用例: $(call chk_file_notext,file)
-define chk_file_notext
-    $(if $(wildcard $1),,$(error $1 not exists in $(CURRDIR)))
-endef
+# テストの結果として作成されるファイル群
+TEST_RES_FILES := $(TEST1_FILE) $(DIFF_FILE) $(ERR_FILE) $(LOG_FILE) $(DETAIL_FILE) $(TIME_FILE)
 
 endif
