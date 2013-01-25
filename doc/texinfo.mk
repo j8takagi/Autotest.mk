3888742173d70889c8748f34506c8ddfdeda2530
j8takagi <j8takagi@nifty.com>
2013-01-25 11:50:53 +0900

    docディレクトリーのMakefileを修正

---
+++ doc/texinfo.mk
@@ -0,0 +1,42 @@
+.PHONY: texinfo-distclean texinfo-clean texinfo-textmp-clean
+
+CP := cp
+DVIPDFMX := dvipdfmx
+INSTALL-INFO := install-info
+MAKEINFO = makeinfo
+MKDIR := mkdir
+RMR := rm -rf
+TEX := ptex
+TEXI2DVI := TEX=$(TEX) texi2dvi
+
+DVIPDFMX_FLAGS ?=
+MAKEINFO_FLAGS ?=
+TEXI2DVI_FLAGS ?= -q --texinfo=@afourpaper
+
+%.info: %.texi
+	$(MAKEINFO) $(MAKEINFO_FLAGS) -o $@ $<
+
+%.html: %.texi $(CSS)
+	$(MAKEINFO) -o $@ --no-split --html --css-include=$(CSS) $<
+
+%_html: %.texi $(CSS)
+	if test ! -e $@; then $(MKDIR) $@; fi
+	$(CP) $(CSS) $@/
+	$(MAKEINFO) -o $@ --html --css-ref=$(CSS) $<
+
+%.pdf: %.dvi
+	$(DVIPDFMX) $(DVIPDFMXFLAGS) $<
+
+%.txt: %.texi
+	$(MAKEINFO) --no-headers --disable-encoding -o $@ $<
+
+%.xml: %.texi
+	@$(MAKEINFO) --docbook -o $@ $<
+
+texinfo-distclean: texinfo-clean
+	@$(RMR) *_html *.info *.html *.pdf  *.dvi *.txt
+
+texinfo-clean: texinfo-textmp-clean
+
+texinfo-textmp-clean:
+	@$(RM) *.aux *.cp *.cps *.fn *.ky *.log *.pg *.pgs *.tmp *.toc *.tp *.vr
